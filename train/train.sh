a9f63f761b820f04bb5577e5bc4b1b86be99cd44
Qingtao Li <qingtaoli@microsoft.com>
2025-02-20 06:58:22 +0000

    Make scripts work with explicit CLI arguments.

--- train/train.sh
+++ train/train.sh
@@ -1,20 +1,16 @@
-export MODEL_PATH='/home/superbench/qingtaoli/models/Phi-3.5-mini-instruct/'
-export SAVE_PATH='/mnt/sdb1/qingtaoli/Phi-3.5-mini-instruct-bitdistiller-wiki3kalpaca5k_ctx20482/'
-export DATA_PATH='/mnt/sdb1/qingtaoli/data-phi-3.5/wikitext-2-generated/mix_wiki_alpaca_8000.json'
-export LOG_PATH='/home/superbench/qingtaoli/models/Phi-3.5-mini-instruct-wiki3kalpaca5k_ctx2048-log2/'
-export CLIP_PATH='/mnt/sdb1/qingtaoli/clip_cache/Phi-3.5-mini-instruct/int2-g64.pt'
-
-# export MODEL_PATH='/home/superbench/qingtaoli/models/Phi-3-mini-4k-instruct/'
-# export SAVE_PATH='/mnt/sdb1/qingtaoli/Phi-3-mini-4k-instruct-bitdistiller-wiki3kalpaca5k_ctx2048/'
-# export DATA_PATH='/mnt/sdb1/qingtaoli/data-phi-3-ctx2048/wikitext-2-generated/mix_wiki_alpaca_8000.json'
-# export LOG_PATH='/home/superbench/qingtaoli/models/Phi-3-mini-4k-instruct-wiki3kalpaca5k_ctx2048-log/'
-# export CLIP_PATH='/mnt/sdb1/qingtaoli/clip_cache/Phi-3-mini-4k-instruct/int2-g64.pt'
-
+## An CLI argument example:
 # export MODEL_PATH='/home/superbench/qingtaoli/models/Llama-2-7b-hf/'
 # export SAVE_PATH='/mnt/sdb1/qingtaoli/Llama-2-7b-bitdistiller/'
 # export DATA_PATH='/mnt/sdb1/qingtaoli/data-llama-2-7b/wikitext-2-generated/mix_wiki_alpaca_8000.json'
 # export LOG_PATH='/home/superbench/qingtaoli/models/Llama-2-7b-log/'
 # export CLIP_PATH='/home/superbench/qingtaoli/BitDistiller/quantization/clip_cache/Llama-2-7b-hf/int2-g64.pt'
+
+export MODEL_PATH=$1
+export SAVE_PATH=$2
+export DATA_PATH=$3
+export LOG_PATH=$4
+export CLIP_PATH=$5
+export MAX_LENGTH=$6
 
 export MASTER_ADDR="localhost"
 export MASTER_PORT="1321"
@@ -22,25 +18,26 @@ export GLOO_SOCKET_IFNAME="lo"
 export NCCL_SOCKET_IFNAME="lo"
 export WANDB_DISABLED=true  
 
-# deepspeed --include localhost:0 
-CUDA_VISIBLE_DEVICES=0 python train.py \
+# CUDA_VISIBLE_DEVICES=0 python \
+deepspeed --include localhost:0,1,2,3 \
+train.py \
     --model_name_or_path $MODEL_PATH \
     --data_path $DATA_PATH \
-    --model_max_length 2048 \
+    --model_max_length $MAX_LENGTH \
     --output_dir $SAVE_PATH \
     --logging_dir $LOG_PATH \
     --num_train_epochs 10 \
     --bf16 True \
     --seed 42 \
-    --per_device_train_batch_size 1 \
-    --per_device_eval_batch_size 4 \
+    --per_device_train_batch_size 8 \
+    --per_device_eval_batch_size 16 \
     --gradient_accumulation_steps 1 \
     --gradient_checkpointing True \
     --evaluation_strategy "steps" \
-    --eval_steps 400 \
+    --eval_steps 100 \
     --load_best_model_at_end True \
     --save_strategy "steps" \
-    --save_steps 800 \
+    --save_steps 100 \
     --save_total_limit 30 \
     --learning_rate 8e-6 \
     --lr_scheduler_type "constant" \
