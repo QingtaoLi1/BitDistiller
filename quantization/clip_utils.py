c13becd50b018b6da0d2872ae151fee9454b48a7
Qingtao Li <qingtaoli@microsoft.com>
2025-07-17 16:03:58 +0800

    [Feature] Add clipping: epicoder.

--- quantization/clip_utils.py
+++ quantization/clip_utils.py
@@ -44,6 +44,8 @@ def get_calib_dataset(datasets="pileval", tokenizer=None, n_samples=128, block_s
         return get_calib_dataset_nemotron(tokenizer=tokenizer, n_samples=n_samples, block_size=block_size, split="code")
     elif datasets == "nemotron_math":
         return get_calib_dataset_nemotron(tokenizer=tokenizer, n_samples=n_samples, block_size=block_size, split="math")
+    elif datasets == "epicoder":
+        return get_calib_dataset_epicoder(tokenizer=tokenizer, n_samples=n_samples, block_size=block_size)
 
 def get_pile_dataset(tokenizer=None, n_samples=512, block_size=512):
     # dataset = load_dataset("json", data_files="/root/model/llm-awq/val.jsonl.zst", split="train")
@@ -185,6 +187,32 @@ def get_calib_dataset_nemotron(tokenizer=None, n_samples=512, block_size=512, sp
         istr = data["input"][0]['content']
         opt = data["output"]
         line = f"{istr}\n\n{opt}"
+        line = line.strip()
+        line_encoded = tokenizer.encode(line)
+        if len(line_encoded) > 512:
+            continue
+        sample = torch.tensor([line_encoded])
+        if sample.numel() == 0:
+            continue
+        samples.append(sample)
+        n_run += 1
+        if n_run == n_samples:
+            break
+    # now concatenate all samples and split according to block size
+    cat_samples = torch.cat(samples, dim=1)
+    n_split = cat_samples.shape[1] // block_size
+    print(f" * Split into {n_split} blocks")
+    return [cat_samples[:, i*block_size:(i+1)*block_size] for i in range(n_split)]
+
+def get_calib_dataset_epicoder(tokenizer=None, n_samples=512, block_size=512):
+    dataset = load_dataset("microsoft/EpiCoder-func-380k", split="train")
+    samples = []
+    n_run = 0
+
+    for data in dataset:
+        istr = data["instruction"]
+        opt = data["output"]
+        line = f"{istr}\n\n<think></think>\n{opt}"
         line = line.strip()
         line_encoded = tokenizer.encode(line)
         if len(line_encoded) > 512:
